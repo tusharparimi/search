c79e0a64e07d35e60cd1e62e7acff7132eb864c6
tusharparimi <tushar.parimi@gmail.com>
2024-12-02 10:49:13 +0530

    euclid dist func

--- functions.py
+++ functions.py
@@ -5,3 +5,6 @@ def clear_console():
 
 def manhatten_distance(p1, p2):
     return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+
+def euclidean_distance(p1, p2):
+    return ((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)**(0.5)
